9900bb7f6cba742e842cc5ca0c3d5a708d42bb2e
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2024-01-23 14:22:38 +0200

    Update to Elixir 1.16

--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   packages = with pkgs; [
-    elixir_1_15
-    (elixir-ls.override {elixir = elixir_1_15;})
-    (livebook.override {elixir = elixir_1_15;})
+    elixir_1_16
+    (livebook.override {elixir = elixir_1_16;})
   ];
 
   shellHook = ''
